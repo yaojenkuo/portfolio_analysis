0d03c691c4401b4303af6bf05e4674e248ccbbab
yaojenkuo <tonykuoyj@gmail.com>
2016-10-27 16:48:31 +0800

    finished testing

--- extract_close_prices.R
+++ extract_close_prices.R
@@ -1,11 +1,13 @@
+# Load necessary packages
 library(quantmod)
 library(magrittr)
-library(rvest)
 library(stringr)
 library(RCurl)
+library(jsonlite)
+#library(xlsx)
 
 # The quantmod part for close price
-extract_close_prices <- function(specified_date, ticker_char) {
+ExtractClosePrices <- function(specified_date, ticker_char) {
   ticker_vector <- ticker_char %>% strsplit(split = ',') %>% unlist()
   ticker_df <- data.frame()
   
@@ -13,52 +15,122 @@ extract_close_prices <- function(specified_date, ticker_char) {
     data <- getSymbols(i,
                        from = specified_date,
                        to = specified_date)
-    ticker <- i
     close_price <- Cl(get(data))[[1]]
-    df_to_bind <- data.frame(ticker = ticker, date = specified_date, close_price = close_price)
-    ticker_df <- rbind(ticker_df, df_to_bind)
-    #Sys.sleep()
+    df_to_rbind <- data.frame(ticker = i, date = specified_date, close_price = close_price)
+    ticker_df <- rbind(ticker_df, df_to_rbind)
+    #Sys.sleep(runif(1, 1, 2))
   }
   return(ticker_df)
 }
 
-date <- "2016-10-21"
-tickers <- "MSFT,AAPL"
-result <- extract_close_prices(specified_date = date, ticker_char = tickers)
+# Extract Yahoo! Finance stats using JSON parser and regex parser
+ScrapeYahooFinanceStats <- function(ticker_char) {
+  ticker_vector <- ticker_char %>% strsplit(split = ',') %>% unlist()
+  number_pattern <- "[0-9]+"
+  digit_number_pattern <- "[0-9]+\\.[0-9]+"
+  metric_df <- data.frame()
+  
+  for (i in ticker_vector) {
+    # JSON parser
+    json_url <- paste("https://query2.finance.yahoo.com/v10/finance/quoteSummary/", i, "?formatted=true&crumb=loFaprfreJS&lang=en-US&region=US&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=finance.yahoo.com", sep = '')
+    json_object <- getURL(json_url)
+    quote_summary <- fromJSON(json_object) %>% unlist()
+    enterprise_value <- quote_summary["quoteSummary.result.defaultKeyStatistics.enterpriseValue.raw"] %>% unname() %>% as.numeric()
+    enterprise_value_to_revenue <- quote_summary["quoteSummary.result.defaultKeyStatistics.enterpriseToRevenue.raw"] %>% unname() %>% as.numeric()
+    enterprise_value_to_ebitda <- quote_summary["quoteSummary.result.defaultKeyStatistics.enterpriseToEbitda.raw"] %>% unname() %>% as.numeric()
+    
+    # regex parser
+    text_url <- paste("https://finance.yahoo.com/quote/", i, "/key-statistics?p=", i, sep = '')
+    text <- getURL(text_url)
+    
+    market_cap_pattern <- "\"marketCap\":\\{\"raw\":[0-9]+,"
+    market_cap <- text %>%
+      str_extract(pattern = market_cap_pattern) %>%
+      str_extract(pattern = number_pattern) %>%
+      unlist() %>%
+      as.numeric()
+    
+    trailing_PE_pattern <- "\"trailingPE\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
+    trailing_PE_vector <- text %>%
+      str_extract(pattern = trailing_PE_pattern) %>%
+      str_extract_all(pattern = digit_number_pattern) %>%
+      unlist()
+    
+    forward_PE_pattern <- "\"forwardPE\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
+    forward_PE_vector <- text %>%
+      str_extract(pattern = forward_PE_pattern) %>%
+      str_extract_all(pattern = digit_number_pattern) %>%
+      unlist()
+    
+    fifty_two_week_high_pattern <- "\"fiftyTwoWeekHigh\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
+    fifty_two_week_high_vector <- text %>%
+      str_extract(pattern = fifty_two_week_high_pattern) %>%
+      str_extract_all(pattern = digit_number_pattern) %>%
+      unlist()
+    
+    fifty_two_week_low_pattern <- "\"fiftyTwoWeekLow\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
+    fifty_two_week_low_vector <- text %>%
+      str_extract(pattern = fifty_two_week_low_pattern) %>%
+      str_extract_all(pattern = digit_number_pattern) %>%
+      unlist()
+    
+    fifty_day_ma_pattern <- "\"fiftyDayAverage\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
+    fifty_day_ma_vector <- text %>%
+      str_extract(pattern = fifty_day_ma_pattern) %>%
+      str_extract_all(pattern = digit_number_pattern) %>%
+      unlist()
+    
+    two_hundred_day_ma_pattern <- "\"twoHundredDayAverage\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
+    two_hundred_day_ma_vector <- text %>%
+      str_extract(pattern = two_hundred_day_ma_pattern) %>%
+      str_extract_all(pattern = digit_number_pattern) %>%
+      unlist()
+    
+    forward_annual_divident_yield_pattern <- "\"dividendYield\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+%\"\\}"
+    forward_annual_divident_yield_vector <- text %>%
+      str_extract(pattern = forward_annual_divident_yield_pattern) %>%
+      str_extract_all(pattern = digit_number_pattern) %>%
+      unlist()
+    
+    trailing_PE <- trailing_PE_vector[1] %>% as.numeric()
+    forward_PE <- forward_PE_vector[1] %>% as.numeric()
+    fifty_two_week_high <- fifty_two_week_high_vector[1] %>% as.numeric()
+    fifty_two_week_low <- fifty_two_week_low_vector[1] %>% as.numeric()
+    fifty_day_ma <- fifty_day_ma_vector[1] %>% as.numeric()
+    two_hundred_day_ma <- two_hundred_day_ma_vector[1] %>% as.numeric()
+    forward_annual_divident_yield <- forward_annual_divident_yield_vector[1] %>% as.numeric()
+    
+    df_to_rbind <- data.frame(ticker = i,
+                              enterprise_value = enterprise_value,
+                              enterprise_value_to_revenue = enterprise_value_to_revenue,
+                              enterprise_value_to_ebitda = enterprise_value_to_ebitda,
+                              market_cap = market_cap,
+                              trailing_PE = trailing_PE,
+                              forward_PE = forward_PE,
+                              fifty_two_week_high = fifty_two_week_high,
+                              fifty_two_week_low = fifty_two_week_low,
+                              fifty_day_ma = fifty_day_ma,
+                              two_hundred_day_ma = two_hundred_day_ma,
+                              forward_annual_divident_yield = forward_annual_divident_yield
+                              )
+    metric_df <- rbind(metric_df, df_to_rbind)
+    Sys.sleep(runif(1, 1, 2))
+  }
+  return(metric_df)
+}
 
-# The crawler part for other metrics
-url <- "https://finance.yahoo.com/quote/AAPL/key-statistics?p=AAPL"
-html_doc <- getURL(url)
-fifty_two_week_high_pattern <- "\"fiftyTwoWeekHigh\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
-fifty_two_week_low_pattern <- "\"fiftyTwoWeekLow\":\\{\"raw\":[0-9]+\\.[0-9]+,\"fmt\":\"[0-9]+\\.[0-9]+\"\\}"
+# Function inputs
+date <- "2016-10-26"
+tickers <- "MSFT,AAPL,FB"
 
-digit_number_pattern <- "[0-9]+\\.[0-9]+"
-fifty_two_week_high <- html_doc %>%
-  str_extract(pattern = fifty_two_week_high_pattern) %>%
-  str_extract_all(pattern = digit_number_pattern) %>% 
-  unlist() %>%
-  as.numeric()
+# Call function with user inputs
+close_price <- ExtractClosePrices(specified_date = date, ticker_char = tickers)
+other_metric <- ScrapeYahooFinanceStats(ticker_char = tickers)
 
-fifty_two_week_low <- html_doc %>%
-  str_extract(pattern = fifty_two_week_low_pattern) %>%
-  str_extract_all(pattern = digit_number_pattern) %>% 
-  unlist() %>%
-  as.numeric()
+# Merge on ticker
+merged_data <- merge(close_price, other_metric)
 
-# Extract market cap, 52-week high and 52-week low from Google Finance
-url <- "https://www.google.com/finance?q=AAPL&ei=a2YQWMGuL5CF0gSP4K-IBw"
-html_doc <- read_html(url)
-xpath_google_finance <- "//tr/td[@class='val']"
-
-google_finance_metrics <- html_doc %>%
-  html_nodes(xpath = xpath_google_finance) %>%
-  html_text() %>%
-  str_replace(pattern = "\\n", replacement = "")
-
-market_cap <- google_finance_metrics[5]
-fifty_two_week <- google_finance_metrics[2] %>%
-  str_split(pattern = " - ") %>%
-  unlist() %>%
-  as.numeric()
-fifty_two_week_high <- fifty_two_week[2]
-fifty_two_week_low <- fifty_two_week[1]
+# Export as csv or excel
+date_for_filename <- date %>% str_replace_all(pattern = '-', replacement = '')
+write.csv(merged_data, file = paste("yahoo_finance_", date_for_filename, '.csv', sep = ''), row.names = FALSE)
+#write.xlsx(merged_data, file = paste("yahoo_finance_", date_for_filename, '.xls', sep = ''), row.names = FALSE)
