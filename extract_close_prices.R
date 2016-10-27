58df1c0e9813cbf653e23cef4dbbd8d131e93bc4
yaojenkuo <tonykuoyj@gmail.com>
2016-10-27 12:51:21 +0800

    added google finance scrape

--- extract_close_prices.R
+++ extract_close_prices.R
@@ -17,6 +17,7 @@ extract_close_prices <- function(specified_date, ticker_char) {
     close_price <- Cl(get(data))[[1]]
     df_to_bind <- data.frame(ticker = ticker, date = specified_date, close_price = close_price)
     ticker_df <- rbind(ticker_df, df_to_bind)
+    #Sys.sleep()
   }
   return(ticker_df)
 }
@@ -44,8 +45,20 @@ fifty_two_week_low <- html_doc %>%
   unlist() %>%
   as.numeric()
 
-# The rvest for Google Finance
+# Extract market cap, 52-week high and 52-week low from Google Finance
 url <- "https://www.google.com/finance?q=AAPL&ei=a2YQWMGuL5CF0gSP4K-IBw"
 html_doc <- read_html(url)
 xpath_google_finance <- "//tr/td[@class='val']"
-data <- html_doc %>% html_nodes(xpath = xpath_google_finance) %>% html_text()
+
+google_finance_metrics <- html_doc %>%
+  html_nodes(xpath = xpath_google_finance) %>%
+  html_text() %>%
+  str_replace(pattern = "\\n", replacement = "")
+
+market_cap <- google_finance_metrics[5]
+fifty_two_week <- google_finance_metrics[2] %>%
+  str_split(pattern = " - ") %>%
+  unlist() %>%
+  as.numeric()
+fifty_two_week_high <- fifty_two_week[2]
+fifty_two_week_low <- fifty_two_week[1]
